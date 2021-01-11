ee214e509d3b263a9906c445053000dbeba49467
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2021-01-11 15:48:46 +0200

    Bug fix

--- src/Console/MigrateTreeMakeCommand.php
+++ src/Console/MigrateTreeMakeCommand.php
@@ -12,6 +12,18 @@ use Illuminate\Support\Composer;
 class MigrateTreeMakeCommand extends MigrateMakeCommand
 {
     /**
+     * The console command signature.
+     *
+     * @var string
+     */
+    protected $signature = 'make:tree-migration {name : The name of the migration}
+        {--create= : The table to be created}
+        {--table= : The table to migrate}
+        {--path= : The location where the migration file should be created}
+        {--realpath : Indicate any provided migration file paths are pre-resolved absolute paths}
+        {--fullpath : Output the full path of the migration}';
+
+    /**
      * Create a new tree migration install command instance.
      *
      * @param  \Illuminate\Filesystem\Filesystem  $files

b8fc01751416f2fd1eab08d10041b136b1925e5f
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2021-01-14 13:05:34 +0200

    Collection toHierarchy() method

--- src/Collection.php
+++ src/Collection.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Collection as BaseCollection;
 
 class Collection extends BaseCollection
 {
+    public function toHierarchy(): BaseCollection
+    {
+        $dictionary = $this->getDictionary();
+
+        uasort($dictionary, fn (Node $a, Node $b) => $a->getLeft() <=> $b->getLeft());
+
+        /** @var Node $node */
+        foreach ($dictionary as $node) {
+            $node->setRelation('children', new BaseCollection);
+        }
+
+        $nestedKeys = [];
+
+        foreach ($dictionary as $node) {
+            $parentKey = $node->getParentKey();
+
+            if (! is_null($parentKey) && array_key_exists($parentKey, $dictionary)) {
+                $node->setRelation('parent', $dictionary[$parentKey]);
+                $dictionary[$parentKey]->children[] = $node;
+                $nestedKeys[] = $node->getKey();
+            }
+        }
+
+        foreach ($nestedKeys as $key) {
+            unset($dictionary[$key]);
+        }
+
+        return new BaseCollection($dictionary);
+    }
 }

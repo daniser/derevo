0d2b500205518f7551994f1c1e3524809b154f9f
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2021-01-22 13:31:12 +0200

    Node factory: in-memory parent-child relationship generation

--- src/NodeFactory.php
+++ src/NodeFactory.php
@@ -5,19 +5,29 @@ declare(strict_types=1);
 namespace TTBooking\Derevo;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Model;
 
 abstract class NodeFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
+    /** @var array<Node|null> $nodes */
+    protected array $nodes = [null];
+
+    public function configure()
     {
-        return [
-            $this->newModel()->getParentColumnName() =>
-                $this->newModel()->newQuery()->inRandomOrder()->firstOrNew()->getKey(),
-        ];
+        return $this->afterMaking(function (Node $node) {
+            shuffle($this->nodes);
+            $node->setRelation('parent', $parent = reset($this->nodes));
+            is_null($parent) || $parent->children[] = $node;
+            $this->nodes[] = $node;
+        });
+    }
+
+    protected function createChildren(Model $model)
+    {
+        parent::createChildren($model);
+
+        if ($model instanceof Node) {
+            $model->children->each(fn (Node $child) => $child->{$child->getParentColumnName()} = $model->getKey());
+        }
     }
 }
